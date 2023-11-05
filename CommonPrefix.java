ee3013e62ba3800691349b3611b139fd30680aa5
Uyen <140712857+Uyennttu@users.noreply.github.com>
2023-11-05 17:51:51 +1000

    Add files via upload
    On*m > find the shortest string and compare each

--- CommonPrefix.java
+++ CommonPrefix.java
@@ -1,25 +1,44 @@
 package dsa;
 
-import java.util.Arrays;
-
 public class CommonPrefix {
 	public static void main(String[] args) {
 		String[] array = { "flowers", "flow", "flight" };
 		String[] array1 = { "dog", "racecar", "car" };
-		System.out.println("The longest common prefix is: " + longestCommonPrefix(array1));
+		int min = getShortestString(array);
+		String minString = getCommonPrefix(array, min);
+		System.out.println("The longest common prefix is: " + minString);
 	}
 
-	public static String longestCommonPrefix(String[] string) {
-		Arrays.sort(string);
-		String first = string[0]; 
-		String last = string[string.length - 1]; 
-		int count = 0;
-		if (first.charAt(count) != last.charAt(count)) {
-			return "None";
+	public static int getShortestString(String[] string) {
+		int shortestString = string[0].length();
+		int shortestStringIndex = 0;
+		for (int i = 0; i < string.length; i++) {
+			if (string[i].length() < shortestString) {
+				shortestString = string[i].length();
+				shortestStringIndex = i;
 			}
-		while (count < first.length() && first.charAt(count) == last.charAt(count)) {
-			count++;
 		}
-		return first.substring(0, count);
+		return shortestStringIndex;
+	}
+
+	public static String getCommonPrefix(String[] string, int shortestStringIndex) {
+		String prefix = "";
+		for (int i = 0; i < string[shortestStringIndex].length(); i++) {
+			char c = string[shortestStringIndex].charAt(i);
+			for (int j = 0; j < string.length; j++) {
+				if (j == shortestStringIndex) {
+					continue;
+				}
+				if (string[j].charAt(i) != c) {
+					return prefix;
+				}
+			}
+			prefix += c;
+		}
+		return prefix;
 	}
 }
+
+// O(n*m)
+// for smaller number of strings
+// Arrays.sort(string)- On^2logn
