c422298813b44ec866c362d9296451b6356036ec
Uyen <140712857+Uyennttu@users.noreply.github.com>
2023-11-12 12:32:18 +0800

    Add files via upload
    use map and set

--- MinimumGap.java
+++ MinimumGap.java
@@ -1,28 +1,25 @@
 package dsa;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MinimumGap {
 	public static void main(String[] args) {
-		int[] array = { 8, 1, 7, 4, 9, 7 };		
-		System.out.println(minimumGap(array));
+		int[] array = { 7, 1, 3, 4, 1, 7 };
+		System.out.println("Minimum Gap: " + minimumGap(array));
 	}
 
 	public static int minimumGap(int[] a) {
-		ArrayList<Integer> listGap = new ArrayList<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
 		int minGap = Integer.MAX_VALUE;
+		// {7:0; 1:1; 3:2; 4:3}
 		for (int i = 0; i < a.length; i++) {
-			for (int j = i + 1; j < a.length; j++) {
-				if (a[i] == a[j]) {
-					int gap = j - i;
-					listGap.add(gap);					
-					minGap = Math.min(minGap, gap);
-				}
+			if (map.containsKey(a[i])) {
+				minGap = Math.min(minGap, i - map.get(a[i]));
 			}
-		}
-		if (listGap.isEmpty()) {
-			return 0;
+			map.put(a[i], i);
+			// {7:5; 1:3; 3:2; 4:3}
 		}
 		return minGap;
+
 	}
 }
--- OddTimes.java
+++ OddTimes.java
@@ -1,36 +1,23 @@
 package dsa;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class OddTimes {
 	public static void main(String[] args) {
-		int[] array = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 100, 400, 15, 15, 15, 15, 15, 15, 15, 15 };
-		Arrays.sort(array); // Onlogn
-		int currentNum = array[0];
-		int count = 0;
-		ArrayList<Integer> oddTimesNumbers = new ArrayList<>(); // On
-
-		for (int num : array) { //On
-			if (num == currentNum) {
-				count++;
-			} else {
-				if (count % 2 != 0) {
-					oddTimesNumbers.add(currentNum);
-				}
-				currentNum = num;
-				count = 1;
+		int[] nums = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 100, 400, 15, 15, 15, 15, 15, 15, 15, 15 };
+		Set<Integer> set = new HashSet<Integer>();
+		for (int value : nums) {
+			if(set.contains(value)) {
+				set.remove(value);
+				continue;
 			}
+			set.add(value);
+			
 		}
-		if (count % 2 != 0) {
-			oddTimesNumbers.add(currentNum);
+		for (Integer integer : set) {
+			System.out.println(integer);
 		}
 
-		System.out.println("Found: ");
-		for (int num : oddTimesNumbers) {
-			System.out.println(num + " ");
-		}
 	}
 }
-//Time: Onlogn
-//Space: On
