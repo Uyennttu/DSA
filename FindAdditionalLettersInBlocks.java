3b21cb40326fab4a9173a6433803c4702a9579a6
Uyen <140712857+Uyennttu@users.noreply.github.com>
2023-11-19 09:11:42 +0800

    Add files via upload
    On

--- FindAdditionalLettersInBlocks.java
+++ FindAdditionalLettersInBlocks.java
@@ -2,39 +2,33 @@ package dsa;
 
 public class FindAdditionalLettersInBlocks {
 	public static void main(String[] args) {
-		String S = "babaa"; // 4
-		String S1 = "bbbab"; // 3
-		String S2 = "bbbaaabbb"; // 3
-		int numOfLetters = getAdditionalLetters(S);
-		System.out.println("Number of Additional Letters: " + numOfLetters);
+		String S = "babaa"; // 4- 3
+		String S1 = "bbbab"; // 3 -4
+		String S2 = "bbbaaabbb"; // 3 -0
+
+		System.out.println("Number of Additional Letters: " + getAdditionalLetters(S));
+		System.out.println("Number of Additional Letters: " + getAdditionalLetters(S1));
+		System.out.println("Number of Additional Letters: " + getAdditionalLetters(S2));
 	}
 
 	public static int getAdditionalLetters(String s) {
-		int currentBlockLength = 1;
-		int maxBlockLength = 0;
+		int countBlock = 1;
+		int max = 0;
 		int additionalLetters = 0;
+		int countChar = 1;
 
 		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i) == s.charAt(i + 1)) {
-				currentBlockLength++;
+			if (s.charAt(i) != s.charAt(i + 1)) {
+				countBlock++;
+				max = Math.max(countChar, max);
+				countChar = 1;
 			} else {
-				maxBlockLength = Math.max(maxBlockLength, currentBlockLength);
-				currentBlockLength = 1;
+				countChar++;
 			}
 		}
-		maxBlockLength = Math.max(maxBlockLength, currentBlockLength);
-		System.out.println("Max Block: " + maxBlockLength);
+		max = Math.max(countChar, max);
+		additionalLetters = max * countBlock - s.length();
 
-		currentBlockLength = 1; 
-		for (int i = 0; i < s.length() - 1; i++) {
-			if (s.charAt(i) == s.charAt(i + 1)) {
-				currentBlockLength++;
-			} else {
-				additionalLetters += maxBlockLength - currentBlockLength; 
-				currentBlockLength = 1;
-			}			
-		}
-		additionalLetters += maxBlockLength - currentBlockLength;
 		return additionalLetters;
 	}
 }
